cde40563a49e72f67d67f9e599004087c8a39ac2
yevhenii-sulim <bees127@ukr.name>
2025-01-25 23:08:40 +0200

    second comit

---
+++ index.js
@@ -0,0 +1,31 @@
+const [burger, menuSvgOpen, menuSvgClose, nav, body] = [
+  ".button-menu",
+  ".menu-svg.open",
+  ".menu-svg.close",
+  ".nav",
+  "body",
+].map((item) => document.querySelector(item));
+burger.addEventListener("click", toggle);
+nav.addEventListener("click", closeMenu);
+
+function toggle() {
+  nav.classList.toggle("is-open");
+  if (nav.classList.contains("is-open")) {
+    menuSvgClose.classList.remove("is-close");
+    menuSvgOpen.classList.add("is-close");
+    body.classList.add("close");
+  } else {
+    menuSvgOpen.classList.remove("is-close");
+    menuSvgClose.classList.add("is-close");
+    body.classList.remove("close");
+  }
+}
+function closeMenu(event) {
+  const target = event.target.closest(".list");
+  if (target) {
+    nav.classList.toggle("is-open");
+    menuSvgClose.classList.toggle("is-close");
+    menuSvgOpen.classList.toggle("is-close");
+    body.classList.toggle("close");
+  }
+}
